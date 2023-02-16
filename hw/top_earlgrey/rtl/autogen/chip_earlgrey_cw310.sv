5eb44a5d1a1f1a63bb2c08907cee6287f30b6320
Pirmin Vogel <vogelpi@lowrisc.org>
2023-02-16 16:19:35 +0100

    WIP [fpga] Stub OBTN to reduce FPGA utilization on CW310
    The utilization of the CW310 FPGA is currently ca. 82%, which could
    disturb SCA experiments.  As OTBN is not relevant for the following
    experiments, this commit stubs it.
    
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/top_earlgrey/rtl/autogen/chip_earlgrey_cw310.sv
+++ hw/top_earlgrey/rtl/autogen/chip_earlgrey_cw310.sv
@@ -1001,6 +1001,7 @@ module chip_earlgrey_cw310 #(
     .KeymgrKmacEnMasking(1),
     .CsrngSBoxImpl(aes_pkg::SBoxImplLut),
     .OtbnRegFile(otbn_pkg::RegFileFPGA),
+    .OtbnStub(1'b1),
     .SecOtbnMuteUrnd(1'b1),
     .SecOtbnSkipUrndReseedAtStart(1'b1),
     .OtpCtrlMemInitFile(OtpCtrlMemInitFile),
