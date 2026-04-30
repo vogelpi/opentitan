e056f5d9544047f444cc010fdff2dd5632637fff
Andrea Caforio <andrea.caforio@lowrisc.org>
2026-04-30 14:13:01 +0200

    [crypto] ML-DSA-87: Computation of the signature vector Z
    This commit adds the routine that computes the signature vector Z with
    shared inputs and checks its infinity norm as part of the ML-DSA sign
    function.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

--- sw/otbn/crypto/mldsa87/sign/mldsa87_sign_ops.s
+++ sw/otbn/crypto/mldsa87/sign/mldsa87_sign_ops.s
@@ -6,6 +6,7 @@
 
 .globl compute_w
 .globl decompose_w
+.globl compute_z
 
 .text
 
@@ -191,5 +192,175 @@ decompose_w:
     addi x7, x7, 1024
     addi x8, x8, 128
     /* End of loop */
+
+  ret
+
+/**
+ * Compute the signature vector Z and check its infinity norm.
+ *
+ * This routine calculates Z = Y + INTT(NTT(C) * NTT(S1)) with arithmetically
+ * shared vectors Y and S1 which are expanded, decoded and converted to
+ * arithmetic shares on on-the-fly (see `expand_mask` and `decode_s`). The
+ * `expand_mask` seed RHO_PRIME is assumed to be passed as 66-bytes in a
+ * 96-byte region as two Boolean shares. The S1 vector is assumed to be passed
+ * in encoded form, i.e., as two 672-byte Boolean shares.
+ *
+ * Each derived signature polynomial Z[i] undergoes a secure infinity norm
+ * check |Z[i]|_inf < 2^19 - BETA before being unmasked.
+ *
+ * @param[in] x2:  DMEM address of the first share of RHO_PRIME seed.
+ * @param[in] x3:  DMEM address of the second share of RHO_PRIME seed.
+ * @param[in] x4:  DMEM address of the challenge polynomial C in NTT domain.
+ * @param[in] x5:  DMEM address of the first share of the encoded vector S1.
+ * @param[in] x6:  DMEM address of the second share of the encoded vector S1.
+ * @param[in] x7:  DMEM address of the vectorized bound 2^19 - BETA (32 bytes).
+ * @param[in] x8:  DMEM address of the resulting signature vector Z.
+ * @param[in] x9:  DMEM address of the polynomial slot 0.
+ * @param[in] x10: DMEM address of the polynomial slot 1.
+ * @param[in] x11: DMEM address of the polynomial slot 2.
+ * @param[in] x12: DMEM address of the polynomial slot 3.
+ * @param[out] w0: 2^256-1 if the norm check passes, 0 otherwise.
+ */
+compute_z:
+  /* Save DMEM address pointers. */
+  addi x13, x2, 0 /* RHO_PRIME_0 (RHO_PRIME share 0) */
+  addi x14, x3, 0 /* RHO_PRIME_1 (RHO_PRIME share 1) */
+  addi x15, x4, 0 /* NTT(C) */
+  addi x16, x5, 0 /* S1_0_enc (encoded S1 share 0) */
+  addi x17, x6, 0 /* S1_1_enc (encoded S1 share 1) */
+
+  /*
+   * Do not save those address pointers which are not overwritten:
+   *   x7:  Bound
+   *   x8:  Z
+   *   x9:  Slot 0
+   *   x10: Slot 1
+   *   x11: Slot 2
+   *   x12: Slot 3
+   */
+
+  /* Loop index for `expand_mask`. */
+  addi x18, x0, 0 /* s */
+
+  /*
+   * Calculate the individual polynomials of the signature vector Z as follows:
+   *
+   * for s in [0, 6]:
+   *   Y0[s], Y1[s] = expand_mask(RHO_PRIME_0, RHO_PRIME_1, s)
+   *   S1_0[s], S1_1[s] = decode_s(S1_0_enc[s], S1_1_enc[s])
+   *
+   *   A0, A1 = NTT(S1_0[s]), NTT(S1_1[s])
+   *   B0, B1 = NTT(C) * W0, NTT(C) * W1
+   *   C0, C1 = INTT(X0), INTT(X1)
+   *   D0, D1 = Y0[s] + C0, Y1[s] + C1
+   *
+   *   if |D0, D1|_inf >= bound:
+   *     return 0
+   *   endif
+   *
+   *   Z[s] = D0 + D1 (unmasking)
+   * endfor
+   *
+   * return 2^256 - 1
+   */
+_compute_z_loop:
+  /* Expand Y0[s] and Y1[s] as arithmetic shares into slots 0 and 1. */
+  addi x2, x9, 0
+  addi x3, x10, 0
+  addi x4, x13, 0
+  addi x5, x14, 0
+  addi x6, x18, 0
+  jal x1, expand_mask
+
+  /* Decode S1_0_enc[s] and S1_1_enc[s] as arithmetic shares into slots 2 and 3. */
+  addi x2, x16, 0
+  addi x3, x17, 0
+  addi x4, x11, 0
+  addi x5, x12, 0
+  jal x1, decode_s
+
+  /* A0 = NTT(S1_0[s]). */
+  addi x2, x11, 0
+  addi x3, x11, 0
+  jal x1, ntt
+
+  /* A1 = NTT(S1_1[s]). */
+  addi x2, x12, 0
+  addi x3, x12, 0
+  jal x1, ntt
+
+  /* B0 = NTT(C) * A0 = NTT(C) * S1_0[s]. */
+  addi x2, x15, 0
+  addi x3, x11, 0
+  addi x4, x11, 0
+  jal x1, poly_mul
+
+  /* B1 = NTT(C) * A1 = NTT(C) * S1_1[s]. */
+  addi x2, x15, 0
+  addi x3, x12, 0
+  addi x4, x12, 0
+  jal x1, poly_mul
+
+  /* C0 = INTT(B0) = INTT(NTT(C) * S1_0[s]). */
+  addi x2, x11, 0
+  addi x3, x11, 0
+  jal x1, intt
+
+  /* C1 = INTT(B1) = INTT(NTT(C) * S1_1[s]). */
+  addi x2, x12, 0
+  addi x3, x12, 0
+  jal x1, intt
+
+  /* Z0 = Y0[s] + C0 = Y0[s] + INTT(NTT(C) * S1_0[s]). */
+  addi x2, x9, 0
+  addi x3, x11, 0
+  addi x4, x9, 0
+  jal x1, poly_add
+
+  /* Z1 = Y1[s] + C1 = Y1[s] + INTT(NTT(C) * S1_1[s]). */
+  addi x2, x10, 0
+  addi x3, x12, 0
+  addi x4, x10, 0
+  jal x1, poly_add
+
+  /* Compute the infinity norm check on the shared signature polynomial and
+     exit the routine if it fails. */
+  addi x2, x9, 0
+  addi x3, x10, 0
+  addi x4, x7, 0
+  jal x1, sec_bound_check
+
+  /* Fail if w0 = 0. */
+  bn.cmp w0, w31, FG0
+  csrrs x2, FG0, x0
+  andi x2, x2, 0x8
+  bne x2, x0, _compute_z_fail
+
+  /* At this point, due to the passed infinity norm check, Z0 and Z1 are not
+     considered sensitive anymore and can be unmasked (see Section 3.2 in [1]).
+       Z[s] = Z0 + Z1 (unmasking). */
+  addi x2, x9, 0
+  addi x3, x10, 0
+  addi x4, x8, 0
+  jal x1, sec_unmask
+
+  /* Increment s, advance S1 and Z pointers. */
+  addi x18, x18, 1
+  addi x16, x16, 96
+  addi x17, x17, 96
+  addi x8, x8, 1024
+
+  /* Loop until all the Z[s] have been computed. */
+  addi x2, x0, 7
+  bne x18, x2, _compute_z_loop
+
+  /* At this point, all the signature polynomials have been computed and have
+     passed the infinity norm check. */
+  bn.not w0, w31
+  ret
+
+  /* Failure case, a signature polynomial has failed the infinity norm check. */
+_compute_z_fail:
+  bn.xor w0, w0, w0
 
   ret
